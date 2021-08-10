3c7e0df4ff9da2ef0119436f83eaeed795afb851
Prashanth2856 <prashanthnani2856@gmail.com>
2021-08-10 19:40:05 +0530

    added all files

---
+++ navbar.js
@@ -0,0 +1,12 @@
+function navBar() {
+  return `<div id="navbar">
+      <h3></h3>
+
+      <div class="options">
+        <h3><a href="recipeByName.html">RecipeByName</a></h3>
+        <h3><a href="RecipeOfTheDay.html">RecipeOfTheDay</a></h3>
+        <h3><a href="latestRecipe.html">LatestRecipe</a></h3>
+      </div>
+    </div>`;
+}
+export default navBar
